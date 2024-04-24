1bfdc7fce5d82a1ff6dadff617cf5f0cba9dd788
lloveland2 <lloveland2@ivytech.edu>
2024-04-23 23:57:55 -0400

    dbURI is working (for now)

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://admin:yCW7Gjx90D0IsUsC@sdev265.66ukqze.mongodb.net/';
-mongoose.connect(dbURI, { useUnifiedTopology: true })
+const dbURI = 'mongodb+srv://AppManager:YUeJqOr4CXjflFqy@sdev265.66ukqze.mongodb.net/?retryWrites=true&w=majority&appName=SDEV265';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
